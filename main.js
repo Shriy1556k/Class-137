948b9d7cafb902573aa7242b89b38f0e2e1a9143
Shriy1556k <shriyansh020409@gmail.com>
2022-09-30 19:50:33 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+object = [];
+var status = " ";
+
+function preload(){
+  video = createVideo('video.mp4');
+}
+
+function setup(){
+  canvas =  createCanvas(480 , 380);
+  canvas.center();
+  video.hide();
+}
+
+function start(){
+  objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+  document.getElementById('status').innerHTML = "Status - Detecting object(approx 2-3 minutes)";
+}
+
+function modelLoaded(){
+  console.log("model is not working");
+  status = true;
+  video.loop();
+  video.speed(1);
+  video.volume(0);
+}
+
+
+function gotResult(results,error){
+  if (error) {
+    console.error(error);
+  }else {
+    console.log(results);
+    objects = results;
+  }
+}
+
+function draw(){
+  image(video , 0 , 0 , 480 , 380);
+
+  if (status != "") {
+    objectDetector.detect(video, gotResult);
+    for ( i=0; i < objects.length ; i++ ){
+      document.getElementById('status').innerHTML = "Objects : Detected objects";
+      document.getElementById('number_of_objects').innerHTML = "No. of objects :" + object.length;
+      fill("#5hh698");
+      percent = floor(objects[i].confidence*100);
+      text(objects[i].label + '' + percent + '%' , objects[i].x+15 , objects[i].y+15 );
+      nofill();
+      stroke("#5hh698");
+      rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);   
+    }
+  }
+}
